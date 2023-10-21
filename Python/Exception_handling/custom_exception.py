a07b401d3c766e72120fe1538997761838186573
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-10-21 12:21:20 +0530

    file handling example

---
+++ Python/Exception_handling/custom_exception.py
@@ -0,0 +1,30 @@
+# i am defining custome error class
+class CustomError(Exception):
+    def __init__(self,custome_error_message):
+        self.custome_error_message = custome_error_message
+        super().__init__(custome_error_message)
+
+
+# Defining a function not a method
+def divide(x,y):
+    if y==0:
+        raise CustomError('bsdk zero he 🤣😂')
+    else:
+        return x / y
+
+
+# now calling the function
+try:
+    result = divide(10,0)
+    print("result of your divide :-",result)
+
+except CustomError as e:
+    print(e)
+
+else:
+    print('successfully divided !')
+
+finally:
+    print('imported taks also has been done !')
+
+

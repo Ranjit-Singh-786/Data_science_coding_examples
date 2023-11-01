0c110382927775ab2b6aeb2632425de1458d6c49
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-11-01 17:38:33 +0530

    email program updated

--- Python/Email_Sender/email_sender.py
+++ Python/Email_Sender/email_sender.py
@@ -1,15 +1,29 @@
 import smtplib
 try:
+    # connecting with gmail server
     server = smtplib.SMTP(host='smtp.gmail.com',port=587)
+    # starting the security protocol
     server.starttls()
-    sender_email = input('Sender email :-')
-    receiver_email = input('Reciever mail :-')
-    server.login("sender_mail_here", "sender_mail_password_here")
 
+    # Ask for Reciever mail
+    receiver_email = input('Enter Reciever mail :- ')
+
+    # put your mail creditional
+    sender_email = 'smartengineer0786@gmail.com'
+    password = '######'
+
+    # login in gmail.com with your creditions
+    server.login(sender_email, password=password)
+
+    #write your mail subject and body
     subject = input('what is your problem 🤣😂  ')
-    body = input('Thoda detail me btao 😜')
+    body = input('Thoda detail me btao 😜 ')
+
     message = f"Subject: {subject}\n\n{body}"
     server.sendmail(sender_email, receiver_email, message)
+    print('Successfully mail has been sent 😉🤞😉')
+
+    # close connection after sending the mail
     server.quit()
 except Exception as e:
     print('Mail was not sent 😒😒!')

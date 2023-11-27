d502daaeb52e0918596613e83591977045dbe14a
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-11-27 15:34:11 +0530

    flask route defining

---
+++ Flask_template/app.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template,url_for,request,jsonify
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    # return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/dharm',methods=['GET','POST'])
+def dharm():
+    return "Dharm asking ,,how are you  second time"
+### ACCESSING PATH OF THIS ROUTE -->  http://localhost:8080/dharm
+
+
+
+@app.route('/jyoti',methods=['GET','POST'])
+def jyoti():
+    return "jyoti asking ,,how are you  second time"
+### ACCESSING PATH OF THIS ROUTE -->  http://localhost:8080/dharm
+
+
+@app.route('/about',methods=['GET','POST'])
+def about():
+    if request.method == 'POST':
+        name = request.form['name']
+        surname = request.form['surname']
+        email = request.form['email']
+        mobile = request.form['mobile']
+        details = {'name':name+' '+surname,'Email':email,'mobile':mobile}
+        return jsonify(details)
+    else:
+        pass
+
+@app.route('/vinod',methods=['GET','POST'])
+def vinod():
+    return "Vinod asking ,,how are you ?"
+### ACCESSING PATH OF THIS ROUTE -->  http://localhost:8080/vinod
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port="8080",debug=True)

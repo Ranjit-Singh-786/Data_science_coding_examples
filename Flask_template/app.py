52917cd0bdeb3e25c1b47803ba7bc1d0ad71d416
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-11-29 10:47:21 +0530

    flask templte update

--- Flask_template/app.py
+++ Flask_template/app.py
@@ -1,24 +1,26 @@
 from flask import Flask,render_template,url_for,request,jsonify
 app = Flask(__name__)
 
+# Defining Home route
 @app.route('/',methods=['GET','POST'])
 def home():
-    # return render_template('index.html')
     return render_template('index.html')
 
+# Defining a root called dharm
 @app.route('/dharm',methods=['GET','POST'])
 def dharm():
     return "Dharm asking ,,how are you  second time"
 ### ACCESSING PATH OF THIS ROUTE -->  http://localhost:8080/dharm
 
 
-
+# Defining a root called jyoti
 @app.route('/jyoti',methods=['GET','POST'])
 def jyoti():
     return "jyoti asking ,,how are you  second time"
 ### ACCESSING PATH OF THIS ROUTE -->  http://localhost:8080/dharm
 
 
+# Defining a root called aout
 @app.route('/about',methods=['GET','POST'])
 def about():
     if request.method == 'POST':
@@ -27,10 +29,11 @@ def about():
         email = request.form['email']
         mobile = request.form['mobile']
         details = {'name':name+' '+surname,'Email':email,'mobile':mobile}
-        return jsonify(details)
+        return render_template('form_data.html',data=details)
     else:
         pass
 
+# Defining a root called vinod
 @app.route('/vinod',methods=['GET','POST'])
 def vinod():
     return "Vinod asking ,,how are you ?"

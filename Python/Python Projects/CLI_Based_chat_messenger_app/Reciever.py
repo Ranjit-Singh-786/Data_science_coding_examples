b70980d974edc5dc09124a028481875eb86b4293
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-05-29 10:29:37 +0530

    python project updated

--- Python/Python Projects/CLI_Based_chat_messenger_app/Reciever.py
+++ Python/Python Projects/CLI_Based_chat_messenger_app/Reciever.py
@@ -2,7 +2,7 @@ import  socket
 import  time 
 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
-my_ip="0.0.0.0"
+my_ip="127.0.0.1"
 # -- 52.2.116.225 -- 
 my_port=9999
 my_address=(my_ip,my_port)
@@ -13,7 +13,10 @@ while 3 > 2 :
     data=s.recvfrom(100)
     # only filetring message 
     new_data=data[0]
+    ip_address = data[1]
     final_msg=new_data.decode('ascii')
     print(final_msg)
+    with open(str(ip_address[0]),'a+')  as file: 
+        file.write(final_msg+'\n')
     # implement file handing print 
-    time.sleep(2)
+    time.sleep(1)
--- Python/Python Projects/CLI_Based_chat_messenger_app/sender.py
+++ Python/Python Projects/CLI_Based_chat_messenger_app/sender.py
@@ -5,12 +5,19 @@ s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 #               ipaddress ,  prototocol 
 # finally s is having capabilty to create UDP socket 
 # target system address
-target_ip="52.2.116.225"
+target_ip="127.0.0.1"
 target_port=9999
 final_target=(target_ip,target_port)
 # taking input from user
-msg=input("Plz enter your message : ")
-# since python3 is supporting minimum encoding 
-new_msg=msg.encode('ascii')
-# final lets send data 
-s.sendto(new_msg,final_target)
+quiet = True
+while quiet:
+    msg=input("Plz enter your message : ")
+    # since python3 is supporting minimum encoding 
+    new_msg=msg.encode('ascii')
+    # final lets send data 
+    s.sendto(new_msg,final_target)
+
+    # code to quiet the sender program
+    user_input = input("do you want to quiet it ? press Y/N : ")
+    if user_input == "y" or user_input=='Y':
+        quiet = False
